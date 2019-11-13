ff91df637468de48a80b28921901bdc96fa95396
Piotr Musielski <piotrmusielski@mail.com>
2019-11-13 07:40:39 +0100

    Main Menu Working

---
+++ Tournament Software/Tournament Software/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament_Software
+{
+    interface Interface1
+    {
+    }
+}
--- Tournament Software/Tournament Software/MainWindow.xaml.cs
+++ Tournament Software/Tournament Software/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Threading;
 
 namespace Tournament_Software
 {
@@ -25,31 +27,162 @@ namespace Tournament_Software
             InitializeComponent();
         }
 
+
         __Player PlayerObject = new __Player();
+        List<string> Nicknames = new List<string>();
+        List<string> DiscordNames = new List<string>();
+
+
+        List<__Player> RandomPlayers = new List<__Player>();
+
+        List<TextBlock> ResultNicks = new List<TextBlock>();
+
+        List<__Player> OutputNamesTeam1 = new List<__Player>();
+        List<__Player> OutputNamesTeam2 = new List<__Player>();
+
+        List<TextBox> PlacingInRounds = new List<TextBox>();
+        List<TextBlock> PointsInRounds = new List<TextBlock>();
+
+        List<TextBlock> LeaderBoardNicks = new List<TextBlock>();
+        List<TextBlock> LeaderBoardDiscords = new List<TextBlock>();
+        List<TextBlock> LeaderBoardScores = new List<TextBlock>();
+
+        List<__Player> PlayersGame1 = new List<__Player>();
+        List<__Player> PlayersGame2 = new List<__Player>();
+        List<__Player> PlayersGame3 = new List<__Player>();
+        List<__Player> PlayersGame4 = new List<__Player>();
+        List<__Player> PlayersGame5 = new List<__Player>();
+
+        #region ListDefinitions
+        void GenerateLists()
+        {
+            Nicknames.Add(Nickname1.Text); Nicknames.Add(Nickname2.Text); Nicknames.Add(Nickname3.Text); Nicknames.Add(Nickname4.Text); Nicknames.Add(Nickname5.Text); Nicknames.Add(Nickname6.Text); Nicknames.Add(Nickname7.Text); Nicknames.Add(Nickname8.Text); Nicknames.Add(Nickname9.Text); Nicknames.Add(Nickname10.Text); Nicknames.Add(Nickname11.Text); Nicknames.Add(Nickname12.Text); Nicknames.Add(Nickname13.Text); Nicknames.Add(Nickname14.Text); Nicknames.Add(Nickname15.Text); Nicknames.Add(Nickname16.Text);
+            DiscordNames.Add(DiscordName1.Text); DiscordNames.Add(DiscordName2.Text); DiscordNames.Add(DiscordName3.Text); DiscordNames.Add(DiscordName4.Text); DiscordNames.Add(DiscordName5.Text); DiscordNames.Add(DiscordName6.Text); DiscordNames.Add(DiscordName7.Text); DiscordNames.Add(DiscordName8.Text); DiscordNames.Add(DiscordName9.Text); DiscordNames.Add(DiscordName10.Text); DiscordNames.Add(DiscordName11.Text); DiscordNames.Add(DiscordName12.Text); DiscordNames.Add(DiscordName13.Text); DiscordNames.Add(DiscordName14.Text); DiscordNames.Add(DiscordName15.Text); DiscordNames.Add(DiscordName16.Text);
+
+            ResultNicks.Add(ResultNick1); ResultNicks.Add(ResultNick2); ResultNicks.Add(ResultNick3); ResultNicks.Add(ResultNick4); ResultNicks.Add(ResultNick5); ResultNicks.Add(ResultNick6); ResultNicks.Add(ResultNick7); ResultNicks.Add(ResultNick8); ResultNicks.Add(ResultNick9); ResultNicks.Add(ResultNick10); ResultNicks.Add(ResultNick11); ResultNicks.Add(ResultNick12); ResultNicks.Add(ResultNick13); ResultNicks.Add(ResultNick14); ResultNicks.Add(ResultNick15); ResultNicks.Add(ResultNick16);
+
+            PlacingInRounds.Add(PlacingInRound1); PlacingInRounds.Add(PlacingInRound2); PlacingInRounds.Add(PlacingInRound3); PlacingInRounds.Add(PlacingInRound4); PlacingInRounds.Add(PlacingInRound5); PlacingInRounds.Add(PlacingInRound6); PlacingInRounds.Add(PlacingInRound7); PlacingInRounds.Add(PlacingInRound8); PlacingInRounds.Add(PlacingInRound9); PlacingInRounds.Add(PlacingInRound10); PlacingInRounds.Add(PlacingInRound11); PlacingInRounds.Add(PlacingInRound12); PlacingInRounds.Add(PlacingInRound13); PlacingInRounds.Add(PlacingInRound14); PlacingInRounds.Add(PlacingInRound15); PlacingInRounds.Add(PlacingInRound16);
+            PointsInRounds.Add(PointsInRound1); PointsInRounds.Add(PointsInRound2); PointsInRounds.Add(PointsInRound3); PointsInRounds.Add(PointsInRound4); PointsInRounds.Add(PointsInRound5); PointsInRounds.Add(PointsInRound6); PointsInRounds.Add(PointsInRound7); PointsInRounds.Add(PointsInRound8); PointsInRounds.Add(PointsInRound9); PointsInRounds.Add(PointsInRound10); PointsInRounds.Add(PointsInRound11); PointsInRounds.Add(PointsInRound12); PointsInRounds.Add(PointsInRound13); PointsInRounds.Add(PointsInRound14); PointsInRounds.Add(PointsInRound15); PointsInRounds.Add(PointsInRound16);
+
+            LeaderBoardNicks.Add(LeaderBoardNick1); LeaderBoardNicks.Add(LeaderBoardNick2); LeaderBoardNicks.Add(LeaderBoardNick3); LeaderBoardNicks.Add(LeaderBoardNick4); LeaderBoardNicks.Add(LeaderBoardNick5); LeaderBoardNicks.Add(LeaderBoardNick6); LeaderBoardNicks.Add(LeaderBoardNick7); LeaderBoardNicks.Add(LeaderBoardNick8); LeaderBoardNicks.Add(LeaderBoardNick9); LeaderBoardNicks.Add(LeaderBoardNick10); LeaderBoardNicks.Add(LeaderBoardNick11); LeaderBoardNicks.Add(LeaderBoardNick12); LeaderBoardNicks.Add(LeaderBoardNick13); LeaderBoardNicks.Add(LeaderBoardNick14); LeaderBoardNicks.Add(LeaderBoardNick15); LeaderBoardNicks.Add(LeaderBoardNick16);
+            LeaderBoardDiscords.Add(LeaderBoardDiscord1); LeaderBoardDiscords.Add(LeaderBoardDiscord2); LeaderBoardDiscords.Add(LeaderBoardDiscord3); LeaderBoardDiscords.Add(LeaderBoardDiscord4); LeaderBoardDiscords.Add(LeaderBoardDiscord5); LeaderBoardDiscords.Add(LeaderBoardDiscord6); LeaderBoardDiscords.Add(LeaderBoardDiscord7); LeaderBoardDiscords.Add(LeaderBoardDiscord8); LeaderBoardDiscords.Add(LeaderBoardDiscord9); LeaderBoardDiscords.Add(LeaderBoardDiscord10); LeaderBoardDiscords.Add(LeaderBoardDiscord11); LeaderBoardDiscords.Add(LeaderBoardDiscord12); LeaderBoardDiscords.Add(LeaderBoardDiscord13); LeaderBoardDiscords.Add(LeaderBoardDiscord14); LeaderBoardDiscords.Add(LeaderBoardDiscord15); LeaderBoardDiscords.Add(LeaderBoardDiscord16);
+            LeaderBoardScores.Add(LeaderBoardScore1); LeaderBoardScores.Add(LeaderBoardScore2); LeaderBoardScores.Add(LeaderBoardScore3); LeaderBoardScores.Add(LeaderBoardScore4); LeaderBoardScores.Add(LeaderBoardScore5); LeaderBoardScores.Add(LeaderBoardScore6); LeaderBoardScores.Add(LeaderBoardScore7); LeaderBoardScores.Add(LeaderBoardScore8); LeaderBoardScores.Add(LeaderBoardScore9); LeaderBoardScores.Add(LeaderBoardScore10); LeaderBoardScores.Add(LeaderBoardScore11); LeaderBoardScores.Add(LeaderBoardScore12); LeaderBoardScores.Add(LeaderBoardScore13); LeaderBoardScores.Add(LeaderBoardScore14); LeaderBoardScores.Add(LeaderBoardScore15); LeaderBoardScores.Add(LeaderBoardScore16);
+        }
+
+        void GenerateOutputLists()
+        {
+
+        }
+
+        void GenerateRandomOutputLists()
+        {
+
+            for (int i = 0; i < RandomPlayers.Count / 2 - 1; i++)
+            {
+                OutputNamesTeam1.Add(RandomPlayers[i]);
+            }
+
+            for (int i = RandomPlayers.Count / 2; i < RandomPlayers.Count; i++)
+            {
+                OutputNamesTeam2.Add(RandomPlayers[i]);
+            }
+
+        }
+
+        #endregion
+
+
+
+
+
+
 
         private void B_SignUp_Click(object sender, RoutedEventArgs e)
         {
-            PlayerObject.GeneratePlayer(PlayerIGN_1.Text, PlayerDN_1.Text);
+            int i = 0;
+            GenerateLists();
+            PlayerObject.ClearLists();
+            foreach (string s in Nicknames)
+            {
+                PlayerObject.GeneratePlayer(Nicknames[i], DiscordNames[i]);
+                i++;
+            }
+
         }
 
         private void B_Generate_Click(object sender, RoutedEventArgs e)
         {
+
+            RandomPlayers = PlayerObject.RandomizePlayers();
+            for (int i = 0; i < 16; i++)
+            {
+                ResultNicks[i].Text = RandomPlayers[i].IGN;
+            }
+
+            string CopyTextA ="LOBBY 1 \r\n";
+            string CopyTextB=" LOBBY 2 \r\n";
+
+            for (int i = 0; i<RandomPlayers.Count/2; i++)
+            {
+                CopyTextA = CopyTextA + "\r\n @" + RandomPlayers[i].DCN + " - " + RandomPlayers[i].IGN;
+                CopyTextB = CopyTextB + "\r \n@" + RandomPlayers[+8].DCN + " - " + RandomPlayers[i+8].IGN;
+            }
+
+            TextBox_Team1.Text = CopyTextA;
+            TextBox_Team2.Text = CopyTextB;
+
+            /*
+            for (int i = ResultNicks.Count / 2; i < ResultNicks.Count; i++)
+            {
+                OutputNamesTeam2.Add(RandomPlayers[i]);
+            }
+
             TextBox_Team1.Text = PlayerObject.FindPlayerByID(0).IGN;
-            ResultNick1.Text = PlayerObject.FindPlayerByID(0).IGN;
+            ResultNick1.Text = PlayerObject.FindPlayerByID(0).IGN;*/
         }
 
         private void B_Submitt_Click(object sender, RoutedEventArgs e)
         {
-            PlayerObject.AssignScores(ResultNick1.Text, System.Convert.ToInt32(ScoreBox1.Text));
-            TextBox_Team2.Text = PlayerObject.FindPlayerByID(0).Score.ToString();
+            //List<TextBox> PlacingInRounds = new List<TextBox>();
+            //List<TextBlock> PointsInRounds = new List<TextBlock>();
+
+            int i = 0;
+            int points = 0;
+            foreach (__Player p in RandomPlayers)
+            {
+                points = PointsFromPlace(PlacingInRounds[i].Text);
+                PointsInRounds[i].Text = points.ToString();    
+                p.Score += points;
+                i++;
+            }
+
+            i = 0;
+            List<__Player> LeaderBoardList = PlayerObject.LeaderBoardList(RandomPlayers);
+
+           foreach (__Player p in LeaderBoardList)
+            {
+
+                LeaderBoardNicks[i].Text = p.IGN;
+                LeaderBoardDiscords[i].Text = p.DCN;
+                LeaderBoardScores[i].Text = p.Score.ToString();
+                i++;
+            }
+
+
+
         }
+
+
+
+
+
 
         private void TextBox_Team2_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        int PointsFromPlace (string place)
+        int PointsFromPlace(string place)
         {
             int test;
             test = System.Convert.ToInt32(place);
@@ -77,8 +210,19 @@ namespace Tournament_Software
             return 0;
         }
 
+        private void B_R1_Click(object sender, RoutedEventArgs e)
+        {  
+            Window WindowR1 = new Round1();
+            WindowR1.Show();
+            
+        }
 
-
+        private void B_R2_Click(object sender, RoutedEventArgs e)
+        {
+            Window WindowR2 = new Round1();
+            WindowR2.Show();
+      
+        }
     }
 
 
@@ -91,9 +235,10 @@ namespace Tournament_Software
         public string IGN;
         public string DCN;
         public int ID;
-        public int Score =0;
+        public int Score = 0;
 
         public List<__Player> PlayersList = new List<__Player>();
+        public List<__Player> InputList = new List<__Player>();
 
         public __Player()
         {
@@ -102,15 +247,16 @@ namespace Tournament_Software
 
         public __Player(string RN, string DN, int id)
         {
-             IGN = RN; //RiotName
-             DCN = DN; //DiscordName
-             ID = id;
+            IGN = RN; //RiotName
+            DCN = DN; //DiscordName
+            ID = id;
         }
 
         public void GeneratePlayer(string RiotName, string DiscordName)
-        { 
-             __Player Player = new __Player(RiotName, DiscordName, IDCounter);
+        {
+            __Player Player = new __Player(RiotName, DiscordName, IDCounter);
             PlayersList.Add(Player);
+            InputList.Add(Player);
             IDCounter++;
 
         }
@@ -141,7 +287,73 @@ namespace Tournament_Software
             }
         }
 
+        public List<__Player> RandomizePlayers()
+        {
+            Shuffle(InputList);
+            return InputList;
+        }
+
+
+
+        public void ClearLists()
+        {
+            InputList.Clear();
+            PlayersList.Clear();
+        }
+
+        private  Random rng = new Random();
+
+        public  void Shuffle(List<__Player> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                __Player value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+       public List<__Player> LeaderBoardList(List<__Player> Input)
+        {
+            List<__Player> target = new List<__Player>();
+
+            foreach (__Player p in Input)
+            {
+                // pierwszy toster
+                if (target.Count == 0)
+                {
+                    target.Add(p);
+                }
+
+                // Jezeli zadaje wiecej - daj go na poczatek
+                else if (p.Score > target[0].Score)
+                {
+                    target.Insert(0, p);
+                }
+
+                // Jezeli zadaje mniej - sortuj
+                else
+                {
+                    bool sorting = true;
+                    int i = target.Count - 1;
+                    while (sorting)
+                    {
+                        if (p.Score <= target[i].Score)
+                        {
+                            target.Insert(i + 1, p);
+                            sorting = false;
+                        }
+                        i--;
+                    }
+                }
+            }
+            return target;
+        }
+
     }
-
-
 }
+
+
